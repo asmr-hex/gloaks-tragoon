57e72e1da702f8df09fe928c1c3533ba63d6e5e2
asmr-hex <c@polygon.pizza>
2021-05-27 11:43:19 -0400

    limping

--- src/main.ts
+++ src/main.ts
@@ -38,10 +38,10 @@ let player
 
 function preload() {
     this.load.image('robot', 'assets/robot.jpeg')
-    // this.load.spritesheet('baby',
-    //     'assets/baby.png',
-    //     { frameWidth: 320, frameHeight: 160 }
-    // )
+    this.load.spritesheet('snob',
+        'assets/s_spritesheet.png',
+        { frameWidth: 120, frameHeight: 120 }
+    )
     // this.load.audio("baby", ["assets/baby.ogg"])
 }
 
@@ -50,22 +50,22 @@ function create() {
     this.add.image(0, 0, 'robot').setOrigin(0)
     text = this.add.text(300, 50, 'robot society', { font: '16px Courier', fill: '#fca7fa' })
 
-    // player = this.physics.add.sprite(100, 450, 'baby')
-    // player.setCollideWorldBounds(true)
+    player = this.physics.add.sprite(100, 450, 'snob')
+    player.setCollideWorldBounds(true)
 
-    // this.anims.create({
-    //     key: 'right',
-    //     frames: this.anims.generateFrameNumbers('baby', { start: 0, end: 2 }),
-    //     frameRate: 10,
-    //     repeat: -1
-    // })
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('snob', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    })
 
-    // this.anims.create({
-    //     key: 'left',
-    //     frames: this.anims.generateFrameNumbers('baby', { start: 3, end: 5 }),
-    //     frameRate: 10,
-    //     repeat: -1
-    // })
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('snob', { start: 4, end: 7 }),
+        frameRate: 10,
+        repeat: -1
+    })
 
     // this.babygoo = this.sound.add("baby")
     // this.babygooing = false
@@ -73,9 +73,17 @@ function create() {
 
 function update() {
     let cursors = this.input.keyboard.createCursorKeys()
+    const speed = 80
 
     if (cursors.left.isDown) {
+        player.setVelocityX(-speed)
+        player.anims.play('left', true)
     }
     else if (cursors.right.isDown) {
+        player.setVelocityX(speed)
+        player.anims.play('right', true)
+    } else {
+        player.setVelocityX(0)
+        player.anims.stop()
     }
 }
